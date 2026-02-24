6e6270abdcd9e29b390e08d5aafab111edfeeb5d
spacegato <pallerad@gmail.com>
2026-02-24 12:21:27 -0500

    added errprint

---
+++ errprint/errprint.h
@@ -0,0 +1,26 @@
+#ifndef ERRPRINT_H
+#define ERRPRINT_H
+
+// Message ANSI Color codes
+#define ERR   "\033[91m"    // Error:   Red
+#define WARN  "\33[93m"    // Warning: Yellow
+#define INFO  "\033[94m"    // Info:    Blue
+#define USAGE "\033[1;36m"  // Usage:   Cyan bold
+#define RESET "\033[0m"     // RESET ANSI
+
+/**
+ * @brief Prints a colored message to stderr. Operates like printf() and is 
+ *        able to substitute variadic arguments into placeholders ('%').
+ *
+ * @param p_color ANSI color code prefix to use (macro defines in header)
+ * @param p_msg format string 
+ * @param ... arguments linked to palceholders in format string
+ */
+void errprint (const char *p_color, const char *p_msg, ...)
+    __attribute__((format(printf, 2, 3)));
+
+    // NOTE: Dunder method provides error checking the same as printf
+    //     2 - points to the format string; 3 is where the variadic args start
+    //     prevents fmt strings that have '%x' with no linking arg
+
+#endif
