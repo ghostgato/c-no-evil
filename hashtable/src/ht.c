19c8b65e602ae049aec7ef491679ab5a17241379
WO1 Pallera, Darren <darren.j.pallera.mil@army.mil>
2026-03-06 01:19:52 -0500

    Edited file to meet barr-c compliance

--- hashtable/src/ht.c
+++ hashtable/src/ht.c
@@ -5,7 +5,7 @@
 #include "ht.h"
 
 #define BALANCE_FACTOR 0.65f
-#define HASH_SEED      5381
+#define HASH_SEED      5381 // seed often used with djb2 hash algorithm
 
 typedef struct ht_node
 {
@@ -30,7 +30,8 @@ default_hash (const char *p_key)
     size_t hash = HASH_SEED;
     int    idx  = 0;
 
-    while ((idx = *p_key++))
+    // need to cast to unsigned to zero extend the char (1 byte) to int (4 bytes)
+    while ((idx = (unsigned char)*p_key++))
     {
         hash = ((hash << 5) + hash) + idx; //  hash = hash * 33 + idx;
     }
@@ -41,24 +42,25 @@ default_hash (const char *p_key)
 static ht_node *
 create_node (const char *p_key, void *p_value)
 {
-    if (!p_key || !p_value)
+    if ((NULL == p_key) || (NULL == p_value))
     {
         fprintf(stderr, "[ERR] Invalid variables to create new node.\n");
         return NULL;
     }
 
     ht_node *p_new = calloc(1, sizeof(ht_node));
-    if (!p_new)
+    if (NULL == p_new)
     {
-        perror("[ERR] Memory allocation fail - create_node()\n");
+        perror("[ERR] Memory allocation fail - create_node()");
         goto END_CREATE;
     }
 
     p_new->p_key = calloc(1, (strlen(p_key) + 1));
-    if (!p_new->p_key)
+    if (NULL == p_new->p_key)
     {
-        perror("[ERR] Memory Allocation Fail - create_node(): p_key\n");
+        perror("[ERR] Memory Allocation Fail - create_node(): p_key");
         free(p_new);
+        p_new = NULL;
     }
     else
     {
@@ -73,7 +75,7 @@ END_CREATE:
 
 static void free_node(ht_node *p_node, del_f del)
 {
-    if (!p_node)
+    if (NULL == p_node)
     {
         fprintf(stderr, "[ERR] No node provided to free - free_node()\n");
         return;
@@ -91,14 +93,18 @@ static void free_node(ht_node *p_node, del_f del)
 
 static inline int needs_resize(ht_t *p_htable)
 {
-    return ((float)p_htable->node_count / p_htable->table_size) > BALANCE_FACTOR;
+    // inline function, parameters have been checked by the calling function
+
+    float load_factor = (float)p_htable->node_count / p_htable->table_size;
+
+    return (BALANCE_FACTOR < load_factor);
 }
 
 static int resize_table(ht_t *p_htable)
 {
-    if (!p_htable)
+    if ((NULL == p_htable) || (0 == p_htable->table_size))
     {
-        fprintf(stderr, "[ERR] No valid table to resize - resizetable()\n");
+        fprintf(stderr, "[ERR] resize_table(): No valid htable provided\n");
         return 0;
     }
 
@@ -108,7 +114,7 @@ static int resize_table(ht_t *p_htable)
 
     ht_node **pp_og_items = p_htable->pp_items;
     ht_node **pp_new_items = calloc(new_size, sizeof(ht_node*));
-    if (!pp_new_items)
+    if (NULL == pp_new_items)
     {
         perror("[ERR] Memory allocation failure - resize_table().\n");
         status = 0;
@@ -151,30 +157,29 @@ static int resize_table(ht_t *p_htable)
 ht_t *
 ht_create (size_t initial_size, hash_f hash)
 {
-    if (initial_size <= 0)
+    if (0 == initial_size)
     {
         fprintf(stderr, "[ERR] Invalid size provided to create new table.\n");
         return NULL;
     }
 
-    // initialize hash table struct
     ht_t *p_htable = calloc(1, sizeof(ht_t));
-    if (!p_htable)
+    if (NULL == p_htable)
     {
-        return NULL;
+        goto END_CREATE;
     }
 
     // initialize array of items
     p_htable->pp_items = calloc(initial_size, sizeof(ht_node*));
-    if (!p_htable->pp_items)
+    if (NULL == p_htable->pp_items)
     {
         perror("[ERR] Memory allocation fail - ht_create() pp_items");
         free(p_htable);
-        return NULL;
+        p_htable = NULL;
+        goto END_CREATE;
     }
     
-
-    if (hash) // if a hash func is provided
+    if (hash) // if a hash func provided
     {
         p_htable->hash_func = hash;
     }
@@ -186,13 +191,14 @@ ht_create (size_t initial_size, hash_f hash)
     p_htable->table_size = initial_size;
     p_htable->node_count = 0;
 
+END_CREATE:
     return p_htable;
 }
 
 void
 ht_destroy (ht_t **pp_htable, del_f del)
 {
-    if (!pp_htable || !*pp_htable) // check at different levels
+    if ((NULL == pp_htable) || (NULL == *pp_htable)) // check at different levels
     {
         fprintf(stderr, "[ERR] Invalid hash table provided.\n");
         return;
@@ -222,20 +228,31 @@ ht_destroy (ht_t **pp_htable, del_f del)
 bool
 ht_put (ht_t *p_htable, const char *p_key, void *p_value)
 {
-    if (!p_htable || !p_key || !p_value)
+    if ((NULL == p_htable) || (NULL == p_key) || (NULL == p_value))
     {
         fprintf(stderr, "[ERR] Invalid variables provided to insert\n");
         return false;
     }
 
     bool   status = true; // start success
-    size_t index  = p_htable->hash_func(p_key) % p_htable->table_size;
+    size_t index  = 0;
+
+    if (0 == p_htable->table_size) // check if need to resize before insertion
+    {
+        if (0 == resize_table(p_htable))
+        {
+            status = false;
+            goto END_PUT;
+        }
+    }
+    
+    index = p_htable->hash_func(p_key) % p_htable->table_size;
 
     // Check if key already exists
     ht_node *p_current = p_htable->pp_items[index];
     while (p_current)
     {
-        if (strcmp(p_current->p_key, p_key) == 0)
+        if (0 == strcmp(p_current->p_key, p_key))
         {
             // if key already exists
             status = false;
@@ -248,7 +265,7 @@ ht_put (ht_t *p_htable, const char *p_key, void *p_value)
     {
         // Create new node
         ht_node *p_new_node = create_node(p_key, p_value);
-        if (!p_new_node)
+        if (NULL == p_new_node)
         {
             fprintf(stderr, "[ERR] Memory allocation failure.\n");
             status = false;
@@ -264,18 +281,24 @@ ht_put (ht_t *p_htable, const char *p_key, void *p_value)
             // Check if resize is needed
             if (needs_resize(p_htable))
             {
-                resize_table(p_htable);
+                if (0 == resize_table(p_htable))
+                {
+                    // will not set status to false as insert succeeded
+                    fprintf(stderr,
+                        "[ERR] Failed to resize table after insertion.\n");
+                }
             }
         }
     }
 
+END_PUT:
     return status;
 }
 
 void *
 ht_get (ht_t *p_htable, const char *p_key)
 {
-    if (!p_htable || !p_key)
+    if ((NULL == p_htable) || (NULL == p_key))
     {
         fprintf(stderr, "[ERR] Invalid variables provided - ht_get()\n");
         return NULL;
@@ -303,7 +326,7 @@ ht_get (ht_t *p_htable, const char *p_key)
 void *
 ht_remove (ht_t *p_htable, const char *p_key)
 {
-    if (!p_htable || !p_key)
+    if ((NULL == p_htable) || (NULL == p_key))
     {
         fprintf(stderr, "[ERR] Invalid variables provided - ht_remove().\n");
         return NULL;
@@ -347,7 +370,7 @@ ht_remove (ht_t *p_htable, const char *p_key)
 void *
 ht_update (ht_t *p_htable, const char *p_key, void *p_new_value)
 {
-    if (!p_htable || !p_key || !p_new_value)
+    if ((NULL == p_htable) || (NULL == p_key) || (NULL == p_new_value))
     {
         fprintf(stderr, "[ERR] Invalid variables provided - ht_update()\n");
         return NULL;
@@ -376,7 +399,7 @@ ht_update (ht_t *p_htable, const char *p_key, void *p_new_value)
 bool
 ht_contains(ht_t *p_htable, const char *p_key)
 {
-    if (!p_htable || !p_key)
+    if ((NULL == p_htable) || (NULL == p_key))
     {
         return false;
     }
@@ -389,9 +412,10 @@ ht_contains(ht_t *p_htable, const char *p_key)
     // incase there are chained items
     while (p_current)
     {
-        if (strcmp(p_current->p_key, p_key) == 0) // check for key match
+        if (0 == strcmp(p_current->p_key, p_key)) // check for key match
         {
             status = true; // key found
+            break;
         }
         p_current = p_current->p_next;
     }
@@ -404,7 +428,7 @@ ht_size (ht_t *p_htable)
 {
     size_t size = 0;
 
-    if (!p_htable)
+    if (NULL == p_htable)
     {
         fprintf(stderr, "[ERR] No valid hash table provided - ht_size()\n");
     }
@@ -419,7 +443,7 @@ ht_size (ht_t *p_htable)
 void
 ht_iter_values (ht_t *p_htable, iter_f iter)
 {
-    if (!p_htable || !iter)
+    if ((NULL == p_htable) || (NULL == iter))
     {
         fprintf(stderr, "[ERR] Invalid variables provided- ht_iter_values()\n");
         return;
@@ -441,27 +465,31 @@ ht_iter_values (ht_t *p_htable, iter_f iter)
 void
 ht_print_keys (ht_t *p_htable)
 {
-    if (!p_htable)
+    if (NULL == p_htable)
     {
         fprintf(stderr, "[ERR] Invalid hashtable provided - print_keys()\n");
         return;
     }
 
-    if (p_htable->table_size < 1)
+    bool status = true;
+
+    if (1 > p_htable->table_size)
     {
         fprintf(stderr, "Hash table empty, Nothing to print.\n");
-        return;
+        status = false;
     }
 
-    printf("Keys in hash table:\n");
-    for (size_t idx = 0; idx < p_htable->table_size; idx++)
+    if (true == status)
     {
-        ht_node *p_curr = p_htable->pp_items[idx];
-
-        while (p_curr)
+        for (size_t idx = 0; idx < p_htable->table_size; idx++)
         {
-            printf("[%zu] %s\n", idx, p_curr->p_key);
-            p_curr = p_curr->p_next;
+            ht_node *p_curr = p_htable->pp_items[idx];
+
+            while (NULL != p_curr)
+            {
+                printf("[%zu] %s\n", idx, p_curr->p_key);
+                p_curr = p_curr->p_next;
+            }
         }
     }
 }
